5bb050ddb7f4f0031451afcc81cee530cf0f71ff
lczyk <marcin.konowalczyk@canonical.com>
2025-12-10 16:32:03 +0000

    feat: 1.85 -> 1.88

--- tests/spread/integration/rustc-1.88/test_help_and_version.sh
+++ tests/spread/integration/rustc-1.88/test_help_and_version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # spellchecker: ignore rootfs rustc
 
-rootfs="$(install-slices rustc-1.85_rustc)"
-chroot "${rootfs}/" rustc-1.85 --help | grep -q "Usage: rustc"
-chroot "${rootfs}/" rustc-1.85 --version | grep -q 'rustc 1.85'
+rootfs="$(install-slices rustc-1.88_rustc)"
+chroot "${rootfs}/" rustc-1.88 --help | grep -q "Usage: rustc"
+chroot "${rootfs}/" rustc-1.88 --version | grep -q 'rustc 1.88'
--- tests/spread/integration/rustc-1.88/test_std.sh
+++ tests/spread/integration/rustc-1.88/test_std.sh
@@ -6,7 +6,7 @@
 #       See: https://github.com/canonical/chisel-releases/issues/761
 arch=$(uname -m)-linux-gnu
 slices=(
-    rustc-1.85_rustc
+    rustc-1.88_rustc
     gcc-15-"${arch//_/-}"_gcc-15
     binutils-"${arch//_/-}"_linker
     libgcc-15-dev_core
@@ -17,5 +17,5 @@ ln -s "${arch}"-ld "${rootfs}"/usr/bin/ld
 
 cp testfiles/test_std.rs "${rootfs}"/test_std.rs
 
-chroot "${rootfs}" rustc-1.85 /test_std.rs -o /test_std
+chroot "${rootfs}" rustc-1.88 /test_std.rs -o /test_std
 chroot "${rootfs}" /test_std
